c81c6f09bb668ad53c8ec4fb42fa357c4da7e65e
jaywcjlove <398188662@qq.com>
2021-06-19 12:33:33 +0800

    website: update example.

--- website/Example.tsx
+++ website/Example.tsx
@@ -1,14 +1,37 @@
 import { useState } from 'react';
-import Only from '../';
+import Only, { OnlyWhenProps } from '../';
+
+type CheckboxProps = {
+  hiddenMode?: OnlyWhenProps['hiddenMode'];
+  value?: OnlyWhenProps['hiddenMode'];
+  onChange?: (evn: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+function Checkbox(props: CheckboxProps) {
+  const { hiddenMode, value, onChange } = props;
+  return (
+    <label>
+      <input type="radio" value={hiddenMode} name="hiddenMode" checked={hiddenMode === value} onChange={onChange} />{' '}
+      {value}
+    </label>
+  );
+}
 
 const App: React.FC = () => {
   const [when, setWhen] = useState(true);
+  const [hiddenMode, setHiddenMode] = useState<OnlyWhenProps['hiddenMode']>('null');
   return (
-    <div>
+    <div style={{ padding: 10, backgroundColor: 'rgb(0 0 0 / 17%)', minWidth: 360, borderRadius: 5 }}>
       <label>
         <input type="checkbox" checked={when} onChange={(e) => setWhen(e.target.checked)} /> when={`${when}`}
       </label>
-      <Only when={when}>
+      <div>
+        <Checkbox hiddenMode={hiddenMode} value="null" onChange={() => setHiddenMode('null')} />
+        <Checkbox hiddenMode={hiddenMode} value="display" onChange={() => setHiddenMode('display')} />
+        <Checkbox hiddenMode={hiddenMode} value="visibility" onChange={() => setHiddenMode('visibility')} />
+        <Checkbox hiddenMode={hiddenMode} value="css" onChange={() => setHiddenMode('css')} />
+      </div>
+      <Only when={when} hiddenMode={hiddenMode} className={hiddenMode === 'css' ? 'w-only-hidden' : ''}>
         <h1>Here I Am</h1>
       </Only>
     </div>
